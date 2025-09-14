d1668c8320554199b96ec3e9c9735755e1c9de7b
Артем Бухряков <artembuhryakov332@gmail.com>
2025-09-14 19:31:16 +0500

    Add project files.

---
+++ Tyuiu.BukhryakovAY.Sprint1.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BukhryakovAY.Sprint1.Task0.V0.Lib;
+namespace Tyuiu.BukhryakovAY.Sprint1.Task0.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(-6, res);
+        }
+    }
+}
---
+++ Tyuiu.BukhryakovAY.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.BukhryakovAY.Sprint1.Task0.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #0                                                              *");
+        Console.WriteLine("* Выполнил: Бухряков Артём Юрьевич | ПКТб-25-1                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение 36/2/9+1-6/2*3          *");
+        Console.WriteLine("* и печатает результат на экране.                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 36/2/9+1-6/2*3                                                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate());
+        Console.ReadLine();
+    }
+}
