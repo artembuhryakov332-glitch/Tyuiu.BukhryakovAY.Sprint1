11c6d09927086457cc43d0c21324e9cb43f6100d
Артем Бухряков <artembuhryakov332@gmail.com>
2025-09-21 12:58:34 +0500

    Task2

--- f/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.Write("Введи символ: ");
-        int x = Console.Read();
-        Console.WriteLine("ASII-код: " + x);
-    }
-}
