af388b3503a316c605c59babb38c242c65e2f2f1
Артем Бухряков <artembuhryakov332@gmail.com>
2025-09-25 00:16:46 +0500

    Task7

--- Tyuiu.BukhryakovAY.Sprint1.Task7.V21.Lib/DataService.cs
+++ Tyuiu.BukhryakovAY.Sprint1.Task7.V21.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.BukhryakovAY.Sprint1.Task7.V21.Lib
         public double Calculate(double x, double y)
         {
             double z;
-            z = ((Math.Pow(y,x)/(Math.Acos(x)-x/3)) + ((Math.Asin(Math.Sqrt(x)) + Math.Acos(y))*Math.Atan(x*y))/(Math.Acos(x) - Math.Asin(y))); 
+            z = (Math.Pow(y, x)) / (Math.Cos(x) - x / 3) + ((Math.Sin(x*x) + Math.Cos(y)) / (Math.Cos(x) - Math.Sin(y))) * Math.Tan(x * y);
             return Math.Round(z,3);
         }
     }
